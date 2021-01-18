ee941db499a4fb5d278db3a330e7dde09e60b9f3
Philip Gheno <philip.gheno@informais.com>
2021-01-18 14:10:17 -0800

    added imageloader

--- gallery/script.js
+++ gallery/script.js
@@ -18,7 +18,9 @@ var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
   layoutMode: 'masonry'
 });
-
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+})
 // filter items on button click
 $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
